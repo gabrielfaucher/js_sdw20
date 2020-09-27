251c9f87762347ec9cba6c6c5e0d6bcdfd235f45
sh0w <gpfaucher@st.noorderpoort.nl>
2020-09-27 16:56:46 +0200

    completed w3/ex2

--- Opdrachten/w3/opdracht2/main.js
+++ Opdrachten/w3/opdracht2/main.js
@@ -4,8 +4,32 @@ const inputColor = document.getElementById('inputColor');
 const headingResults = document.getElementById('results');
 const buttonSave = document.querySelector('button');
 
+// Callback on form save
 buttonSave.addEventListener('click', () => {
-    document.cookie = `name = ${inputName.value};`;
-    document.cookie = `color = ${inputColor.value};`;
-    console.log(document.cookie);
+    // Store input in localStorage
+    localStorage.name = inputName.value;
+    localStorage.color = inputColor.value;
+
+    // Set background color to localStorage
+    document.body.style.backgroundColor = localStorage.color;
 });
+
+// Track page visits on refresh
+window.addEventListener('load', () => {
+    if (localStorage.visits == undefined) {
+        console.log('visits is not defined');
+        localStorage.visits = 0;
+    } else {
+        localStorage.visits++;
+    }
+});
+
+// Display results
+headingResults.innerHTML = `Opgeslagen naam is: ${localStorage.name} en de pagina is ${localStorage.visits} keer bezocht`;
+
+// Set background color to localStorage value
+document.body.style.backgroundColor = localStorage.color;
+
+// Set input placeholder to values in localStorage
+inputColor.placeholder = localStorage.color;
+inputName.placeholder = localStorage.name;
