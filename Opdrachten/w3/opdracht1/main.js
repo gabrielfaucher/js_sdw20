0f047694c53667e048ab68caff9f6806ac8bc7fb
sh0w <gpfaucher@st.noorderpoort.nl>
2020-09-27 18:24:10 +0200

    completed w3/ex1

--- Opdrachten/w3/opdracht1/main.js
+++ Opdrachten/w3/opdracht1/main.js
@@ -2,27 +2,36 @@
 const inputName = document.getElementById('inputName');
 const inputColor = document.getElementById('inputColor');
 const headingResults = document.getElementById('results');
+const headingVisitedResults = document.getElementById('resultsVisited');
 const buttonSave = document.querySelector('button');
 const bodyStyle = document.body.style;
+
+// Get cookie values
+let colorValue = document.cookie.replace(
+    /(?:(?:^|.*;\s*)color\s*\=\s*([^;]*).*$)|^.*$/,
+    '$1'
+);
+let nameValue = document.cookie.replace(
+    /(?:(?:^|.*;\s*)name\s*\=\s*([^;]*).*$)|^.*$/,
+    '$1'
+);
+let visitsCookie = document.cookie.replace(
+    /(?:(?:^|.*;\s*)visits\s*\=\s*([^;]*).*$)|^.*$/,
+    '$1'
+);
 
 // If cookies aren't empty, we set the background color
 // This way it's set on page load instead of only on
 // button click
 if (document.cookie != 'name=; color=') {
-    let colorValue = document.cookie.replace(
-        /(?:(?:^|.*;\s*)color\s*\=\s*([^;]*).*$)|^.*$/,
-        '$1'
-    );
-    let nameValue = document.cookie.replace(
-        /(?:(?:^|.*;\s*)name\s*\=\s*([^;]*).*$)|^.*$/,
-        '$1'
-    );
-
     // Set placeholder and body background
     bodyStyle.backgroundColor = colorValue;
     inputName.placeholder = nameValue;
     inputColor.placeholder = colorValue;
-    results.innerHTML = `The name you stored is: ${nameValue}`;
+
+    // Display name & page visits
+    headingResults.innerHTML = `The name you stored is: ${nameValue}`;
+    headingVisitedResults.innerHTML = `and you visited this page ${visitsCookie} times`;
 } else {
     console.log('no cookies found!');
 }
@@ -30,11 +39,15 @@ if (document.cookie != 'name=; color=') {
 // Create cookies on button click
 buttonSave.addEventListener('click', () => {
     let expires = genExpirationDate(7);
+
+    // Set cookies with expiration date
     document.cookie = `name = ${inputName.value}; expires=${expires}]`;
     document.cookie = `color = ${inputColor.value}; expires=${expires}`;
 
+    // Set background color and display name & page visits
     bodyStyle.backgroundColor = inputColor.value;
-    results.innerHTML = `The name you stored is: ${nameValue}`;
+    headingResults.innerHTML = `The name you stored is: ${nameValue}`;
+    headingVisitedResults.innerHTML = `and you visited this page ${visitsCookie} times`;
 });
 
 // Generate expiration date
@@ -48,28 +61,15 @@ function genExpirationDate(days) {
     return nextweek;
 }
 
-// Save & track page visits
-function trackVisits() {
-    let visitsAmount = document.cookie.replace(
-        /(?:(?:^|.*;\s*)visits\s*\=\s*([^;]*).*$)|^.*$/,
-        '$1'
-    );
-    console.log(visitsAmount);
-    var visits;
-    if (
-        (typeof visitsAmount == '' && visitsAmount != 0) ||
-        visitsAmount == undefined
-    ) {
+// Track page visits on refresh
+var visits;
+window.addEventListener('load', () => {
+    if (visitsCookie == '' || visitsCookie == 'NaN') {
         visits = 0;
-        console.log('visitsAmount is nothing');
-        console.log(visits);
-        document.cookie = `visits = ${visits};`;
-    } else {
-        console.log('visitsAmount exists!');
-        // visits++;
-        console.log(visits);
-        document.cookie = `visits = ${visits};`;
+        document.cookie = `visits=${visits}`;
+    } else if (parseInt(visitsCookie) >= 0) {
+        visits = parseInt(visitsCookie);
+        visits++;
+        document.cookie = `visits=${visits}`;
     }
-}
-
-window.addEventListener('load', trackVisits);
+});
